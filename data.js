f0beacc3e20ffde1dbb3eece1c8d386b97d6ca55
Michael Edlinger <mkeedlinger@gmail.com>
2017-06-21 18:04:06 -0600

    WIP data handler

--- data.js
+++ data.js
@@ -1,15 +1,22 @@
-module.exports = {
-  propertyExists: function(obj, nesting) {
-    for (var i = 0; i < nesting.length; i++) {
-      if (!obj || !obj.hasOwnProperty(nesting[i])) {
-        return {
-          name: nesting[i],
-          index: i
-        } // returns where things stopped being found
-      }
-      obj = obj[nesting[i]]
+const fs = require('fs')
+const config = require('./config')
+const promise = require('bluebird')
+
+exports.data = {}
+
+exports.propertyExists = function(obj, nesting) {
+  for (var i = 0; i < nesting.length; i++) {
+    if (!obj || !obj.hasOwnProperty(nesting[i])) {
+      return {
+        name: nesting[i],
+        index: i
+      } // returns where things stopped being found
     }
-    return true
-  },
-  data: {}
+    obj = obj[nesting[i]]
+  }
+  return true
+}
+
+exports.saveToDisk = function() {
+  fs.write
 }
