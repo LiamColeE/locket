9e592369e013e3e5876a1ae3847512aa1d1aaedb
Michael Edlinger <mkeedlinger@gmail.com>
2017-06-23 15:55:29 -0600

    prettier

--- crypto.js
+++ crypto.js
@@ -1,5 +1,8 @@
 const ts = require('triplesec')
 
-exports.hashPass = function hashPass(pass){
-  return (new ts.hash.SHA512()).update(new ts.WordArray.from_utf8(pass)).finalize().to_hex()
+exports.hashPass = function hashPass(pass) {
+  return new ts.hash.SHA512()
+    .update(new ts.WordArray.from_utf8(pass))
+    .finalize()
+    .to_hex()
 }
