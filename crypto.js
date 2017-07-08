365a614dc2ca8e0d4812ae91f759ef2f7366a196
Michael Edlinger <mkeedlinger@gmail.com>
2017-07-08 14:07:58 -0600

    added token generator

--- crypto.js
+++ crypto.js
@@ -1,8 +1,13 @@
 const ts = require('triplesec')
+const crypto = require('crypto')
 
 exports.hashPass = function hashPass(pass) {
   return new ts.hash.SHA512()
     .update(new ts.WordArray.from_utf8(pass))
     .finalize()
     .to_hex()
+}
+
+exports.genToken = function genToken(){
+  return crypto.randomBytes(64).toString('base64')
 }
