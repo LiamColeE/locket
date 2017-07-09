8239c0fe1fdcfb5cc7afda999d54fb3f9f27ebc1
Michael Edlinger <mkeedlinger@gmail.com>
2017-07-08 21:09:01 -0600

    made info send in an object also fixed bad delete reference

--- api/tokens.js
+++ api/tokens.js
@@ -24,31 +24,26 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
   // get a new token with the name that was passed in
-  if (typeof req.body === 'string') {
-    let newToken = {
-      name: req.body,
-      id: data.data.next_token_id++,
-      issue_time: new Date().toISOString(),
-      expire_time: new Date(
-        Date.now() + data.data.settings.password_timeout * 1000
-      ).toISOString(),
-      value: crypto.genToken(),
-      creator_ip: req.ip
-    }
-
-    data.data.tokens.current.push(newToken)
-    data.saveToDisk()
-
-    res.status(201).json({
-      id: newToken.id,
-      expire_time: newToken.expire_time,
-      value: newToken.value
-    })
-  } else {
-    res
-      .status(400)
-      .json({ error: 'Body should be a string that is stringified JSON.' })
+  // TODO all object validation
+  let newToken = {
+    name: req.body.name,
+    id: data.data.next_token_id++,
+    issue_time: new Date().toISOString(),
+    expire_time: new Date(
+      Date.now() + data.data.settings.password_timeout * 1000
+    ).toISOString(),
+    value: crypto.genToken(),
+    creator_ip: req.ip
   }
+
+  data.data.tokens.current.push(newToken)
+  data.saveToDisk()
+
+  res.status(201).json({
+    id: newToken.id,
+    expire_time: newToken.expire_time,
+    value: newToken.value
+  })
 })
 
 router.get('/revoked', function(req, res) {
@@ -58,14 +53,14 @@ router.get('/revoked', function(req, res) {
 
 router.delete('/:id', function(req, res) {
   // revoke a token
-  let tokenIndex = data.data.accounts.findIndex(token => {
+  let tokenIndex = data.data.tokens.current.findIndex(token => {
     return token.id == req.params.id
   })
 
   if (tokenIndex === -1) {
     res.status(404).end()
   } else {
-    let token = data.data.tokens.current.splice(tokenIndex, 1)
+    let token = data.data.tokens.current.splice(tokenIndex, 1)[0]
     token.revoked_time = new Date().toISOString()
 
     data.data.tokens.revoked.push(token)
