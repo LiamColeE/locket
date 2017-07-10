5c007079d910f28e27262ec592e0824874ef0214
Michael Edlinger <mkeedlinger@gmail.com>
2017-07-09 19:18:59 -0600

    refacted auth not great, but better

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 const config = require('./config')
-const fs = require('fs')
 const api = require('./api/')
 const data = require('./data')
 const crypto = require('./crypto')
@@ -16,24 +15,30 @@ app.get('/', function(req, res) {
   res.send('This will someday be a little Vue app.')
 })
 
+// Auth
+function auth(req, res, next) {
+  data.checkTokenTimes()
+
+  let dat = data.data
+
+  let tokenVal = dat.tokens.current.some(function(token) {
+    return token.value === req.cookies.token
+  })
+
+  let tempPass =
+    !dat.pass_hash && req.body.passHash === crypto.hashPass(config.tempPass)
+
+  let pass = dat.pass_hash && dat.pass_hash === req.body.passHash
+
+  if (tokenVal || tempPass || pass) {
+    next()
+  } else {
+    res.status(401).end()
+  }
+}
+
 // use api routes
-app.use(
-  '/api',
-  function(req, res, next) {
-    data.checkTokenTimes()
-    if (
-      data.data.tokens.current.includes(req.cookies.token) ||
-      (!data.data.pass_hash &&
-        req.body.passHash === crypto.hashPass(config.tempPass)) ||
-      (data.data.pass_hash && data.data.pass_hash === req.body.passHash)
-    ) {
-      next()
-    } else {
-      res.status(401).send()
-    }
-  },
-  api
-)
+app.use('/api', auth, api)
 
 ////
 // start!
